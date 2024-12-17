12322c5cb46c60f0784df2c9293e164645d57d2c
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2024-12-18 06:31:56 +0700

    updated profile

--- do_an_cuoi_khoa_react/src/components/FirstColumn/Setting/ChangePro/ChangeProfile.js
+++ do_an_cuoi_khoa_react/src/components/FirstColumn/Setting/ChangePro/ChangeProfile.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./ChangeProfile.css";
 import Navigation from "../../../Navigation/Navigation";
 import { Button, DatePicker, Input, message } from "antd";
+import axios from "axios";
 
 const ChangeProfile = () => {
   const userId = localStorage.getItem("loggedInUserId"); // Lấy userId từ localStorage
@@ -14,11 +15,38 @@ const ChangeProfile = () => {
       message.error("Plese fill in your new name");
       return;
     }
+    try {
+      const { data: userData } = await axios.get(
+        `http://localhost:3000/users/${userId}`
+      );
+      const updatedUserName = { ...userData, nickname: newName };
+      await axios.put(`http://localhost:3000/users/${userId}`, updatedUserName);
+      message.success("Name changed successfully!");
+      setNewName("");
+    } catch (error) {
+      console.error("Error when changing name:", error);
+      message.error("Failed to change name. Please try again later.");
+    }
   };
   const handleChangeEmail = async () => {
     if (!newEmail) {
       message.error("Plese fill in your new email");
       return;
+    }
+    try {
+      const { data: userData } = await axios.get(
+        `http://localhost:3000/users/${userId}`
+      );
+      const updatedUserEmail = { ...userData, email: newEmail };
+      await axios.put(
+        `http://localhost:3000/users/${userId}`,
+        updatedUserEmail
+      );
+      message.success("Email changed successfully!");
+      setNewEmail("");
+    } catch (error) {
+      console.error("Error when changing email:", error);
+      message.error("Failed to change email. Please try again later.");
     }
   };
   const handleChangeDateOfBirth = async () => {
@@ -26,45 +54,57 @@ const ChangeProfile = () => {
       message.error("Plese fill in your new date of birth");
       return;
     }
+    try {
+      const { data: userData } = await axios.get(
+        `http://localhost:3000/users/${userId}`
+      );
+      const updatedUserDateOfBirth = {
+        ...userData,
+        dateOfBirth: newDateOfBirth.format("YYYY-MM-DD"),
+      };
+      await axios.put(
+        `http://localhost:3000/users/${userId}`,
+        updatedUserDateOfBirth
+      );
+      message.success("Date of Birth changed successfully!");
+      setNewDateOfBirth(null); // Reset field sau khi cập nhật thành công
+    } catch (error) {
+      console.error("Error when changing date of birth:", error);
+      message.error("Failed to change date of birth. Please try again later.");
+    }
   };
   return (
     <div className="body-setting-profile">
       <Navigation />
       <div className="changePro-container">
         <h3>Change Profile</h3>
-        <div>
-          Name
-          <Input
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-            placeholder="Enter new name"
-          />
-          <Button type="primary" onClick={handleChangeName}>
-            Update Name
-          </Button>
-        </div>
-        <div>
-          Email
-          <Input
-            value={newEmail}
-            onChange={(e) => setNewEmail(e.target.value)}
-            placeholder="Enter new email"
-          />
-          <Button type="primary" onClick={handleChangeEmail}>
-            Update Name
-          </Button>
-        </div>
-        <div>
-          Date of Birth
-          <DatePicker
-            value={newDateOfBirth} // Bind value từ state
-            onChange={(date) => setNewDateOfBirth(date)} // Cập nhật state khi chọn ngày
-            placeholder="Select your date of birth"
-          />
-          <Button type="primary" onClick={handleChangeDateOfBirth}>
-            Update Name
-          </Button>
-        </div>
+        Name
+        <Input
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+          placeholder="Enter new name"
+        />
+        <Button type="primary" onClick={handleChangeName}>
+          Update Name
+        </Button>
+        Email
+        <Input
+          value={newEmail}
+          onChange={(e) => setNewEmail(e.target.value)}
+          placeholder="Enter new email"
+        />
+        <Button type="primary" onClick={handleChangeEmail}>
+          Update Email
+        </Button>
+        Date of Birth
+        <DatePicker
+          value={newDateOfBirth} // Bind value từ state
+          onChange={(date) => setNewDateOfBirth(date)} // Cập nhật state khi chọn ngày
+          placeholder="Select your date of birth"
+        />
+        <Button type="primary" onClick={handleChangeDateOfBirth}>
+          Update Date Of Birth
+        </Button>
       </div>
     </div>
   );
