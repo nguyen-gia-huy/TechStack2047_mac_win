ba062ce712e15f88ebd9ee82f61d4bfc0c19ec7e
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2025-06-24 15:46:07 +0700

    comit

--- quizz-test/backend/configs/authConfig.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const authConfig = {
-   secretKey: process.env.JWT_SECRET || 'your_secret_key'
-}
--- quizz-test/src/pages/ChangePassword/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ChangePassword = () => {
-	return <h1>ChangePassword</h1>;
-};
-
-export default ChangePassword;
